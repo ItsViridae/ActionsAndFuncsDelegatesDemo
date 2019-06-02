d8e6f779054ace42c8de7b81024da751b0bc3723
Scott Comeaux <viridaecode@gmail.com>
2019-06-02 10:42:41 -0500

    Add project files.

---
+++ ActionsAndFuncsDelegatesDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ActionsAndFuncsDelegatesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
